1f6ee54b698e2486fd4b1dd62b816f9269b93615
William Mansky <mansky1@uic.edu>
2023-03-18 14:12:41 -0500

    exclusive algebra

---
+++ theories/algebra/excl.v
@@ -0,0 +1,25 @@
+From iris.algebra Require Import excl.
+From iris_ora.algebra Require Export ora.
+From iris.prelude Require Import options.
+
+Section excl.
+Context {A : ofe}.
+
+Instance excl_orderN : OraOrderN (excl A) := dist.
+Instance excl_order : OraOrder (excl A) := equiv.
+
+Definition excl_ora_mixin : OraMixin (excl A).
+Proof.
+  split; try apply _; try done.
+  - intros ???? Hv Ho.
+    by rewrite -Ho in Hv; apply exclusiveN_r in Hv.
+  - eauto.
+  - apply dist_S.
+  - by intros ???? ->.
+  - apply equiv_dist.
+  - inversion 1.
+Qed.
+
+Canonical Structure exclR := Ora (excl A) excl_ora_mixin.
+
+End excl.
